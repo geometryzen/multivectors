2d4e3d0e8ed5709b45959c8d7c8e3c00001b1b48
David Geo Holmes <geometryzen@gmail.com>
2023-01-06 12:04:29 -0500

    Matrix3

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -85,7 +85,7 @@ const options =
 export default [
     options,
     {
-        input: 'dist/esm/index.d.ts',
+        input: 'dist/esm/types/index.d.ts',
         output: [{ file: pkg.types, format: "esm" }],
         plugins: [dts()],
     }
