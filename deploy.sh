c5317b9dd1be918a1034d6dc0fae1a01f24c38e9
David Geo Holmes <geometryzen@gmail.com>
2024-04-03 06:41:26 -0400

    'Evergreen'

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+npm install
+npm update
+npm run build
+npm run lint
+npm run test
+npm run check
+git status
+git add --all
+echo "Please enter a commit message"
+read message
+git commit -m "'$message'"
+git push origin master
+npm run release
+npm run docs
+npm run pages
